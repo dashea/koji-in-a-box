ad936ff994f727cf1f73b827998563d493db80f1
David Shea <reallylongword@gmail.com>
2022-09-16 12:23:46 -0400

    Create a CA certificate
    Add a few lines to the README about the choice of using SSL
    authentication.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 This project defines a multi-architecture koji build system that can be run on a single machine as a set of non-root containers.
 This is a pretty weird thing to want to do.
+
+## An overview of the guts
+
+### Authentication
+
+Koji provides two choices for authentication: kerberos or SSL certificates.
+This project uses SSL certificates.
+
+A major downside of using SSL certificates for authentication is that the root of authentication is a file stored on the host system.
+As defined in [generate_secrets.sh](generate_secrets.sh), the CA certificate and private key are stored on the filesystem with no password.
+Anyone with access to the root certificate files will be able to take control of the build system.
+
+The upside of using SSL certificates is that it's not kerberos.
--- generate_secrets.sh
+++ generate_secrets.sh
@@ -1,8 +1,24 @@
 #!/bin/sh -e
 
+# CA variables, replace as desired
+CA_COUNTRY="US"
+CA_STATEORPROVINCE="Massachusetts"
+CA_LOCALITY="Boston"
+CA_ORGANIZATION="Koji in a Box"
+
 # Create a password for the koji-hub to postgres connection
 # Do no replace the password if it is already set. The value is only used by the postgres
 # container when the database is initially created. Replacing the postgres password after the database has been
 # initialized will require replacing the secret AND changing the password in psql.
 podman secret inspect koji-postgres-password >/dev/null 2>&1 || \
     openssl rand -base64 32 | podman secret create koji-postgres-password -
+
+# Create the certificate authority that will act as the root for authenticating all services.
+# Files are saved as koji_ca_cert.crt and koji_ca_cert.key
+if [ ! -f koji_ca_cert.crt ]; then
+    openssl req -new -x509 -extensions v3_ca \
+        -subj "/C=${CA_COUNTRY}/ST=${CA_STATEORPROVINCE}/L=${CA_LOCALITY}/O=${CA_ORGANIZATION}/CN=koji-ca" \
+        -days 3650 \
+        -newkey rsa:2048 -passout 'pass:' -keyout koji_ca_cert.key \
+        -out koji_ca_cert.crt
+fi
